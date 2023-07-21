decfc2c932d747e7ef6cfa0397ff4db5c438666e
meesdekker.xyz <me@meesdekker.xyz>
2023-07-21 12:58:26 +0200

    🐛 fix(global.c): free env vector when exit is true in free_global function

--- src/utils/global.c
+++ src/utils/global.c
@@ -6,7 +6,7 @@
 /*   By: mdekker/jde-baai <team@codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/21 04:17:23 by mdekker/jde   #+#    #+#                 */
-/*   Updated: 2023/07/21 04:25:46 by mdekker/jde   ########   odam.nl         */
+/*   Updated: 2023/07/21 12:51:05 by mdekker/jde   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@ extern t_global	g_data;
 /**
  * @brief Frees the global variables
  *
- * @param exit If is true, the exit status will be freed
+ * @param exit If is true, the exit status and env vector will be freed
  */
 void	free_global(bool exit)
 {
 	ft_vec_free(&g_data.tokens);
-	ft_vec_free(&g_data.env);
 	if (exit)
+	{
+		ft_vec_free(&g_data.env);
 		free(g_data.exit_status);
+	}
 }
