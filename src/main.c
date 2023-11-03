ec77401fe8faa4832b5ec6fe21fb34f7d5219e78
Julius de Baaij <julius@debaaij.com>
2023-11-03 21:43:29 +0100

    Catching cmd==NULL in executing function and made function map for main()

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mdekker/jde-baai <team@codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/07/12 14:11:01 by mdekker/jde   #+#    #+#                 */
-/*   Updated: 2023/11/02 17:17:16 by mdekker/jde   ########   odam.nl         */
+/*   Updated: 2023/11/03 21:42:00 by mdekker/jde   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ static void	debug(void)
 	printf("\033[0m");
 }
 
-static void	soft_exit(t_shell *data, char *input)
+static void	soft_exit(char *input, t_shell *data)
 {
 	write_err(data);
 	free(input);
@@ -31,12 +31,34 @@ static void	soft_exit(t_shell *data, char *input)
 	vec_init(&data->token_vec, 5, sizeof(t_token), clear_token);
 }
 
+static bool function_map(char *input, t_shell *data)
+{
+	bool	(*function[5])(t_shell *);
+	int		i;
+
+	function[0] = parser;
+	function[1] = check_tokens;
+	function[2] = expand_tokens;
+	function[3] = group_token_vec;
+	function[4] = executor;
+
+	if (!lexer(input, data))
+		return (false);
+	i = -1;
+	while (i++ < 4)
+	{
+		if (!function[i](data))
+			return (soft_exit(input, data), false);
+	}
+	return (true);
+}
+
 /**
  * @brief The main loop of the program
  *
  * @param vec The vector to store the token_vec in
  */
-static int	loop(t_shell *data)
+static void	loop(t_shell *data)
 {
 	char	*input;
 
@@ -49,80 +71,27 @@ static int	loop(t_shell *data)
 			continue ;
 		}
 		add_history(input);
-		if (!lexer(input, data))
-		{
-			soft_exit(data, input);
+		if (!function_map(input, data))
 			continue ;
-		}
-		if (!parser(data))
-		{
-			soft_exit(data, input);
-			continue ;
-		}
-		if (!check_tokens(data))
-		{
-			soft_exit(data, input);
-			continue ;
-		}
-		if (!expand_tokens(data))
-		{
-			soft_exit(data, input);
-			continue ;
-		}
-		print_vector(&data->token_vec, print_token);
-		if (!group_token_vec(data))
-		{
-			soft_exit(data, input);
-			continue ;
-		}
-		if (!executor(data))
-		{
-			soft_exit(data, input);
-			continue ;
-		}
 		free(input);
 		free_shell(data, false);
 		vec_init(&data->token_vec, 5, sizeof(t_token), clear_token);
 	}
-	return (0);
 }
 
 int	main(int ac, char **av, char **env)
 {
 	t_shell	*data;
 
+	(void)av;
 	if (DEBUG)
 		debug();
+	if (ac != 1)
+	{
+		write(2, "Too many arguments\n", 20);
+		return (1);
+	}
 	data = init_shell(env, true);
-	if (ac == 2)
-	{
-		if (!lexer(av[1], data))
-			return (write_err(data), free_shell(data, true),
-				g_signal.exit_status);
-		if (!parser(data))
-			return (write_err(data), free_shell(data, true),
-				g_signal.exit_status);
-		// if (!operator_split(data))
-		// 	return (write_err(data), free_shell(data, true),
-		// 		g_signal.exit_status);
-		if (!check_tokens(data))
-			return (write_err(data), free_shell(data, true),
-				g_signal.exit_status);
-		if (!group_token_vec(data))
-			return (write_err(data), free_shell(data, true),
-				g_signal.exit_status);
-		if (!executor(data))
-			return (write_err(data), free_shell(data, true),
-				g_signal.exit_status);
-		free_shell(data, true);
-		return (0); // change this to return built_in_exit
-	}
-	else if (ac == 1)
-		return (loop(data));
-	else
-	{
-		printf("Too many arguments");
-		free_shell(data, true);
-	}
+	loop(data);
 	return (0);
 }
