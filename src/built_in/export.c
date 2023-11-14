0dfc93e2cf96365d7a380398c785a8c17e5fc8fd
Mees Dekker <me@meesdekker.xyz>
2023-11-14 17:31:48 +0100

    Changed export to be norm compliant

--- src/built_in/export.c
+++ src/built_in/export.c
@@ -6,13 +6,31 @@
 /*   By: mdekker/jde-baai <team@codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/31 22:20:10 by mdekker/jde   #+#    #+#                 */
-/*   Updated: 2023/11/07 15:47:25 by mdekker       ########   odam.nl         */
+/*   Updated: 2023/11/14 17:28:42 by mdekker       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <minishell.h>
 
 extern t_signal	g_signal;
+
+/**
+ * @brief Takes in a string and if it is a valid string, it will return a
+ *  duplicate or if the input equals to NULL it
+ * will return a duplicate of an empty string
+ */
+static char	*string_handler(char *input)
+{
+	char	*str;
+
+	if (!input)
+		str = ft_strdup("");
+	else
+		str = ft_strdup(input);
+	if (!str)
+		exit_mini("string_handler", 1);
+	return (str);
+}
 
 static bool	validate_input(t_group *group)
 {
@@ -39,6 +57,14 @@ static bool	validate_input(t_group *group)
 	return (true);
 }
 
+/**
+ * @brief Helper function for vec_find_f
+ *
+ * @param item The item to compare
+ * @param key The key to compare with
+ * @return true If the key and item key are equal
+ * @return false If the key and item key are not equal
+ */
 bool	compare_env_key(void *item, void *key)
 {
 	char	*str;
@@ -49,18 +75,21 @@ bool	compare_env_key(void *item, void *key)
 	return (false);
 }
 
+/**
+ * @brief Function to add a env to the env vector
+ *
+ * @param env_vec The vector to add the env token to
+ * @param key The key of the env token
+ * @param value The value of the env token
+ */
 static void	add_env(t_vector *env_vec, char *key, char *value)
 {
 	t_env	*token;
 	char	*key_dup;
 	char	*value_dup;
 
-	key_dup = ft_strdup(key);
-	if (!value)
-		value_dup = ft_strdup("");
-	else
-		value_dup = ft_strdup(value);
-	printf("key: %s, value: %s\n", key_dup, value_dup);
+	key_dup = string_handler(key);
+	value_dup = string_handler(value);
 	if (!key_dup || !value_dup)
 	{
 		if (key_dup)
@@ -78,6 +107,14 @@ static void	add_env(t_vector *env_vec, char *key, char *value)
 		return ;
 }
 
+/**
+ * @brief The main export builtin function
+ *
+ * @param group The group struct that contains the arguments
+ * @param env_vec The vector that contains the enviroment variables
+ *
+ * !TODO: Add the ability to export without arguments aka print
+ */
 void	ft_export(t_group *group, t_vector *env_vec)
 {
 	size_t	i;
@@ -96,10 +133,7 @@ void	ft_export(t_group *group, t_vector *env_vec)
 		if (token)
 		{
 			free(token->value);
-			if (!env[1])
-				token->value = ft_strdup("");
-			else
-				token->value = ft_strdup(env[1]);
+			token->value = string_handler(env[1]);
 		}
 		else
 			add_env(env_vec, env[0], env[1]);
