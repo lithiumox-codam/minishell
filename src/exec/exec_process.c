14d704bebe00aa5ca4a953cbb0118893b030b215
Julius De Baaij <jde-baai@student.codam.nl>
2023-11-22 16:25:24 +0100

    perror exec_err

--- src/exec/exec_process.c
+++ src/exec/exec_process.c
@@ -6,7 +6,7 @@
 /*   By: mdekker/jde-baai <team@codam.nl>             +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/08/19 16:08:08 by mdekker/jde   #+#    #+#                 */
-/*   Updated: 2023/11/22 16:18:12 by mdekker/jde   ########   odam.nl         */
+/*   Updated: 2023/11/22 16:25:13 by mdekker/jde   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ static void	close_unused(t_process type, t_group *group)
 	if (type == LEFT || type == MIDDLE)
 	{
 		if (close(group->right_pipe[0]) == -1)
-			perror("minishell: right_pipe[0]");
+			exec_err(NULL, PERR);
 	}
 	if (type == RIGHT || type == MIDDLE)
 	{
 		if (close(group->left_pipe[1]) == -1)
-			perror("minishell: left_pipe[1]");
+			exec_err(NULL, PERR);
 	}
 }
 
@@ -33,12 +33,12 @@ static void	dup_redirects(t_group *group)
 	if (group->in_red != -1)
 	{
 		if (dup2(group->in_red, STDIN_FILENO) == -1)
-			perror("minishell: in_red");
+			exec_err(NULL, PERR);
 	}
 	if (group->out_red != -1)
 	{
 		if (dup2(group->out_red, STDOUT_FILENO) == -1)
-			perror("minishell: out_red");
+			exec_err(NULL, PERR);
 	}
 }
 
@@ -52,16 +52,16 @@ void	dup_fd(t_group *group, t_process type)
 	if (type == LEFT || type == MIDDLE)
 	{
 		if (dup2(group->right_pipe[1], STDOUT_FILENO) == -1)
-			perror("minishell: right_pipe[1]");
+			exec_err(NULL, PERR);
 		if (close(group->right_pipe[1]) == -1)
-			perror("minishell");
+			exec_err(NULL, PERR);
 	}
 	if (type == RIGHT || type == MIDDLE)
 	{
 		if (dup2(group->left_pipe[0], STDIN_FILENO) == -1)
-			perror("minishell: left_pipe[0]");
+			exec_err(NULL, PERR);
 		if (close(group->left_pipe[0]) == -1)
-			perror("minishell: left_pipe[0]");
+			exec_err(NULL, PERR);
 	}
 	dup_redirects(group);
 }
